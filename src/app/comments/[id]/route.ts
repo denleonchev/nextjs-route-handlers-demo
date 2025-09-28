c5251d3ceda1c41fec6de07aebb643f93f215731
denleonchev <den.leonchev@gmail.com>
2025-09-28 19:59:00 +0000

    Add PATCH method

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -7,3 +7,13 @@ export async function GET(_request: Request, { params }: { params: Promise<{id: 
     return Response.json(foundComment);
 }
 
+export async function PATCH(request: Request, { params }: { params: Promise<{id: string }>}) {
+    const patchBody = await request.json();
+    const { id } = await params;
+    const foundComment = comments.find((comment) => comment.id === parseInt(id))
+    if (foundComment) {
+        foundComment.text = patchBody.text;
+    }
+
+    return Response.json(foundComment);
+}
