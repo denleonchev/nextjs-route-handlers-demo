5b76688349039c539a58b26ea545e504e449f253
denleonchev <den.leonchev@gmail.com>
2025-09-29 07:26:58 +0000

    Use headers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,14 @@
-export async function GET() {
-    return new Response('Profile data')
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+    // const headers = new Headers(request.headers);
+    // const auth = headers.get("Authorization");
+    const auth = (await headers()).get("Authorization")
+
+    return new Response(`<h1>Authorization: ${auth}</h1>`, {
+        headers: {
+            "Content-type": "text/html"
+        }
+    })
 }
