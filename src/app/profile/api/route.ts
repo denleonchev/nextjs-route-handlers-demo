28199ed1f1a4f4f5f05d1df9f281e5d853f0ae2b
denleonchev <den.leonchev@gmail.com>
2025-09-29 08:19:11 +0000

    Playing some more with cookies

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,14 +1,21 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
     // const headers = new Headers(request.headers);
     // const auth = headers.get("Authorization");
     const auth = (await headers()).get("Authorization")
+    const theme = request.cookies.get("theme");
+    console.log("theme - ", theme);
+
+    const cookieStore = await cookies();
+    cookieStore.set('resultsPerPage', '20');
+    console.log("resultsPerPage - ", cookieStore.get('resultsPerPage'));
 
     return new Response(`<h1>Authorization: ${auth}</h1>`, {
         headers: {
-            "Content-type": "text/html"
+            "Content-type": "text/html",
+            "Set-Cookie": "theme=dark"
         }
     })
 }
