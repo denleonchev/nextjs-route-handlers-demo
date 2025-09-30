741d6d7dd4a897573949f74a2fbcc0eb4efec569
denleonchev <den.leonchev@gmail.com>
2025-09-30 13:58:32 +0000

    Play with cookies and headers in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,23 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
-    return NextResponse.redirect(new URL("/", request.url))
-}
+// export function middleware(request: NextRequest) {
+//     return NextResponse.redirect(new URL("/", request.url))
+// }
 
-export const config = {
-    matcher: "/profile"
+// export const config = {
+//     matcher: "/profile"
+// }
+
+export function middleware(request: NextRequest) {
+    const response = NextResponse.next();
+    const themePreference = request.cookies.get("theme")?.value;
+
+    if (!themePreference) {
+        response.cookies.set("theme", "dark" );
+    }
+
+    response.headers.set("custom-header", "custom-value");
+    
+    return response
 }
